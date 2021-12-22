c2d26909435852a073fbc950b416c81c3203adb4
kazu0617 <kazu0617@protonmail.com>
2021-12-22 16:40:19 +0900

    v1.1.0
    Replace: Copy -> Open button
    
    Click Open to OpenWorld without focus.
    
    Open ボタンをクリックするとワールドをプリロードします。移動する場合はJoinボタンで。

--- SessionTweaks/Properties/AssemblyInfo.cs
+++ SessionTweaks/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("MessageCopy")]
+[assembly: AssemblyTitle("SessionTweaks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MessageCopy")]
+[assembly: AssemblyProduct("SessionTweaks")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- SessionTweaks/SessionTweaks.cs
+++ SessionTweaks/SessionTweaks.cs
@@ -12,7 +12,7 @@ namespace SessionTweaks
     {
         public override string Name => "SesisonTweaks";
         public override string Author => "kazu0617";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/kazu0617/SessionTweaks"; // this line is optional and can be omitted
 
         public override void OnEngineInit()
@@ -37,52 +37,82 @@ namespace SessionTweaks
 
                         //Create Orb Button
                         Slot DuplicatedSlot_Orb = c.Parent.Duplicate();
-                        DuplicatedSlot_Orb.GetComponent<RectTransform>().AnchorMin.Value = new float2(0.6f, 0f);
-                        DuplicatedSlot_Orb.GetComponent<RectTransform>().AnchorMax.Value = new float2(0.79f, 1f);
+                        DuplicatedSlot_Orb.GetComponent<RectTransform>().AnchorMin.Value = new float2(0.4f, 0f);
+                        DuplicatedSlot_Orb.GetComponent<RectTransform>().AnchorMax.Value = new float2(0.59f, 1f);
 
                         DuplicatedSlot_Orb.GetComponent<Button>().Destroy();
                         DuplicatedSlot_Orb.GetComponent<Image>().Destroy();
 
                         DuplicatedSlot_Orb.AttachComponent<Image>();
+                        DuplicatedSlot_Orb.GetComponentInChildren<Text>().Content.Value = "Orb";
 
-                        var newBtn_Orb = DuplicatedSlot_Orb.AttachComponent<Button>();
-                        newBtn_Orb.LocalPressed += (IButton b, ButtonEventData _) =>
+                        DuplicatedSlot_Orb.AttachComponent<Button>().LocalPressed += (IButton b, ButtonEventData _) =>
                         {
-                            SessionInfo sessionInfo = __instance.SessionInfo;
+                            SessionInfo sessionInfo = __instance.Engine.WorldAnnouncer.GetInfoForSessionId(__instance.SessionInfo.SessionId) ?? __instance.SessionInfo;
                             World world = __instance.LocalUser.World.WorldManager.FocusedWorld;
-                            world.RunSynchronously((Action)(() =>
+                            world.RunSynchronously(() =>
                             {
                                 Slot slot = world.RootSlot.LocalUserSpace.AddSlot("World Orb");
                                 WorldOrb worldOrb = slot.AttachComponent<WorldOrb>();
                                 worldOrb.ActiveSessionURLs = sessionInfo.GetSessionURLs();
                                 worldOrb.ActiveUsers.Value = sessionInfo.JoinedUsers;
                                 worldOrb.WorldName = sessionInfo.Name;
+                                worldOrb.ThumbnailTexURL = new Uri(sessionInfo.Thumbnail);
+                                UniLog.Log("ThumbnailTexURL: " + sessionInfo.Thumbnail);
                                 slot.PositionInFrontOfUser();
-                            }));
+                            });
                         };
-                        DuplicatedSlot_Orb.GetComponentInChildren<Text>().Content.Value = "Orb";
 
-                        //Create Copy Button
-                        Slot DuplicatedSlot_Copy = c.Parent.Duplicate();
-                        DuplicatedSlot_Copy.GetComponent<RectTransform>().AnchorMin.Value = new float2(0.4f, 0f);
-                        DuplicatedSlot_Copy.GetComponent<RectTransform>().AnchorMax.Value = new float2(0.59f, 1f);
+                        //Create Open Button
+                        Slot DuplicatedSlot_Open = c.Parent.Duplicate();
+                        DuplicatedSlot_Open.GetComponent<RectTransform>().AnchorMin.Value = new float2(0.6f, 0f);
+                        DuplicatedSlot_Open.GetComponent<RectTransform>().AnchorMax.Value = new float2(0.79f, 1f);
 
-                        DuplicatedSlot_Copy.GetComponent<Button>().Destroy();
-                        DuplicatedSlot_Copy.GetComponent<Image>().Destroy();
+                        DuplicatedSlot_Open.GetComponent<Button>().Destroy();
+                        DuplicatedSlot_Open.GetComponent<Image>().Destroy();
 
-                        DuplicatedSlot_Copy.AttachComponent<Image>();
+                        DuplicatedSlot_Open.AttachComponent<Image>();
+                        DuplicatedSlot_Open.GetComponentInChildren<Text>().Content.Value = "Open";
 
-                        var newBtn_Copy = DuplicatedSlot_Copy.AttachComponent<Button>();
-                        newBtn_Copy.LocalPressed += (IButton b, ButtonEventData _) =>
+                        DuplicatedSlot_Open.AttachComponent<Button>().LocalPressed += (IButton b, ButtonEventData _) =>
                         {
-                            SessionInfo sessionInfo = __instance.SessionInfo;
+                            SessionInfo sessionInfo = __instance.Engine.WorldAnnouncer.GetInfoForSessionId(__instance.SessionInfo.SessionId) ?? __instance.SessionInfo;
                             World world = __instance.LocalUser.World.WorldManager.FocusedWorld;
-                            world.RunSynchronously((Action)(() =>
+                            world.RunSynchronously(() =>
                             {
-                                b.World.InputInterface.Clipboard.SetText("neos-session:///" + sessionInfo.SessionId);
-                            }));
+                                if (__instance.SessionInfo == null || sessionInfo.HasEnded)
+                                    return;
+                                Userspace.OpenWorld(new WorldStartSettings()
+                                {
+                                    URIs = sessionInfo.GetSessionURLs(),
+                                    GetExisting = true,
+                                    AutoFocus = false
+                                });
+                            });
                         };
-                        DuplicatedSlot_Copy.GetComponentInChildren<Text>().Content.Value = "Copy";
+
+                        ////Create CopySessionURL Button
+                        //Slot DuplicatedSlot_Copy = c.Parent.Duplicate();
+                        //DuplicatedSlot_Copy.GetComponent<RectTransform>().AnchorMin.Value = new float2(0.6f, 0f);
+                        //DuplicatedSlot_Copy.GetComponent<RectTransform>().AnchorMax.Value = new float2(0.79f, 0.49f);
+
+                        //DuplicatedSlot_Copy.GetComponent<Button>().Destroy();
+                        //DuplicatedSlot_Copy.GetComponent<Image>().Destroy();
+
+                        //DuplicatedSlot_Copy.AttachComponent<Image>();
+
+                        //DuplicatedSlot_Copy.AttachComponent<Button>().LocalPressed += (IButton b, ButtonEventData _) =>
+                        //{
+                        //    SessionInfo sessionInfo = __instance.SessionInfo;
+                        //    World world = __instance.LocalUser.World.WorldManager.FocusedWorld;
+                        //    world.RunSynchronously(() =>
+                        //    {
+                        //        if (!__instance.InputInterface.IsClipboardSupported)
+                        //            return;
+                        //        b.World.InputInterface.Clipboard.SetText("http://cloudx.azurewebsites.net/open/session/" + sessionInfo.SessionId);
+                        //    });
+                        //};
+                        //DuplicatedSlot_Copy.GetComponentInChildren<Text>().Content.Value = "Copy URL";
                     }
                 }
 
